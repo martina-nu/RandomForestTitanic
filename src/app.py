aec2af5e93cd69c4e8bab73f7a58e0d286ec2085
martina-nu <martinanu98@gmail.com>
2022-08-01 07:11:28 +0000

    add xgb project

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@ import pandas as pd
 import pickle
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+import xgboost as xgb
 
 
 df = pd.read_csv('data/interim/clean_titanic.csv')
@@ -13,10 +14,21 @@ y = df['Survived']
 
 X_train,X_test,y_train,y_test = train_test_split(X,y,stratify=y, random_state=15)
 
-model = RandomForestClassifier(random_state=11, n_estimators=1000, min_samples_split=2, min_samples_leaf=2, max_features='sqrt',max_depth=70, bootstrap=True)
+# Random Forest
 
-model.fit(X_train,y_train)
+rf_model = RandomForestClassifier(random_state=11, n_estimators=1000, min_samples_split=2, min_samples_leaf=2, max_features='sqrt',max_depth=70, bootstrap=True)
 
-filename = 'models/finalized_model.sav'
+rf_model.fit(X_train,y_train)
 
-pickle.dump(model, open(filename,'wb'))
+filename = 'models/rf_finalized_model.sav'
+
+pickle.dump(rf_model, open(filename,'wb'))
+
+#XGBoost
+
+xgb_cl2 = xgb.XGBClassifier(colsample_bytree = 0.5, gamma= 1, learning_rate= 0.05, max_depth= 7, reg_lambda= 1, scale_pos_weight= 1, subsample= 0.8, objective="binary:logistic")
+xgb_cl2.fit(X_train, y_train)
+
+filename = 'models/xgb_finalized_model.sav'
+
+pickle.dump(xgb_cl2, open(filename,'wb'))
